64638cade39634890b21f27f6593a9629f419a83
林泽佳 <15999549759@qq.com>
2020-04-15 20:25:06 +0800

    4.15

---
+++ src/com/lin/processcontrol/Shuchu.java
@@ -0,0 +1,25 @@
+package com.lin.processcontrol;
+
+import jdk.nashorn.internal.ir.IfNode;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Shuchu {
+    public static void main(String[] args) {
+        System.out.printf("%.2f\n", 2.323232);
+        System.out.printf("%x\n", 500);
+        System.out.printf("%o\n", 500);
+        System.out.printf("%s\n", "sfuck");   //
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please input");
+        String var = scanner.nextLine();
+        System.out.println(var);
+        if (var.equals("cys") ) {
+            System.out.println("hello");
+        } else {
+            System.out.println("error");
+        }
+        scanner.close();
+    }
+}
