7a08e009f8407ff50b830b1aeb5fdd5084df984e
linOnePlus <872690700@qq.com>
2020-04-26 23:06:24 +0800

    4.26 common

---
+++ src/myexception/CommonsLogging.java
@@ -0,0 +1,10 @@
+package myexception;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class CommonsLogging {
+    public static void main(String[] args) {
+        Log log = LogFactory.getLog(CommonsLogging.class);
+    }
+}
