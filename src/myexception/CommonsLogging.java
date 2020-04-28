636655c49f17d6adbb48a999a91ef6e8a21c8ee3
linOnePlus <872690700@qq.com>
2020-04-28 17:35:47 +0800

    20.4.28 学到反射

--- src/myexception/CommonsLogging.java
+++ src/myexception/CommonsLogging.java
@@ -1,10 +1,26 @@
 package myexception;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 public class CommonsLogging {
+
     public static void main(String[] args) {
         Log log = LogFactory.getLog(CommonsLogging.class);
+        log.info("start");
+        log.warn("end");
+        new CommonsLogging().testOrdinary();
+        System.out.println(Math.random());
+    }
+    public void testOrdinary(){
+        System.out.println(getClass());
+        Log log =LogFactory.getLog(getClass());//实例方法用getclass获取
+        try {
+            int x = 10/0;
+        }catch (ArithmeticException e){
+            log.error("除0异常",e);
+        }
     }
 }
+
