2cbda5e1c12a3acdb6fee3f0477e19aed85ea346
linOnePlus <872690700@qq.com>
2020-05-17 22:28:19 +0800

    5.15

--- src/reflection/Dynamic_proxy.java
+++ src/reflection/Dynamic_proxy.java
@@ -5,24 +5,57 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 //运行时期动态创建接口实现类
+//没搞懂
 public class Dynamic_proxy {
     public static void main(String[] args) {
-        InvocationHandler invocationHandler =  new InvocationHandler() {
+        InvocationHandler invocationHandler = new InvocationHandler() {
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                if (method.getName().equals("hello")){
+                System.out.println(method);
+                if (method.getName().equals("hello")) {
                     System.out.println("这是动态代理的内容");
                 }
-                return  null;
+                return null;
             }
 
         };
-        hello h =(hello) Proxy.newProxyInstance(hello.class.getClassLoader(), new Class[]{hello.class}, invocationHandler);
+        hello h = (hello) Proxy.newProxyInstance(hello.class.getClassLoader(), new Class[]{hello.class}, invocationHandler);
         h.hello();
+
+
+        //不用内部类的写法
+        InvocationHandler invocationHandler2 = new MyProxy(new helloimpl());
+        hello hello3 = (hello) Proxy.newProxyInstance(hello.class.getClassLoader(), new Class[]{hello.class}, invocationHandler2);
+        hello3.hello();
 
     }
 }
 
-interface  hello{
+interface hello {
     void hello();
+}
+
+class helloimpl implements hello {
+
+    @Override
+    public void hello() {
+        System.out.println("hello proxy");
+    }
+
+}
+
+class MyProxy implements InvocationHandler {
+
+    public hello h2;
+
+    public MyProxy(hello h2) {
+        this.h2 = h2;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+        method.invoke(h2);
+        return null;
+    }
 }
